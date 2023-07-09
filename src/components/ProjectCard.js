02dacedc0be71cef876e5d0c1e8a5ca82705d633
Veikko1983 <veikko1983@gmail.com>
2023-07-09 10:34:44 +0300

    minor projectCard textfield update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -7,7 +7,8 @@ export const ProjectCard = ({ title, description, imgUrl, url }) => {
         <img src={imgUrl} />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          {/* task: span text field is not composed according to the text in the image */}
+          <span className="align-items-center">{description}</span>
           <div>
             <h5>
               <a target="_blank" style={{ color: "white" }} href={url}>

3fe9636823d2f47af42c9269426ddff8a4f96d05
Veikko1983 <veikko1983@gmail.com>
2023-07-18 11:58:39 +0300

    NavBar.js swr import(commented for future use) upd

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,32 @@ import navIcon1 from "../assets/img/nav-icon1.svg";
 //import { HashLink } from "react-router-hash-link";
 //import { BrowserRouter as Router } from "react-router-dom";
 import RetroHitCounter from "react-retro-hit-counter";
+
+//import useSWR from "swr";
+
+// async function fetcher(...args) {
+//   const res = await fetch(...args)
+//   return res.json()
+// }
+
+// export default function ViewCounter({ slug, blogPage = false }) {
+//   const { data } = useSWR(`/api/views/${slug}`, fetcher)
+//   const views = new Number(data?.total)
+
+//   useEffect(() => {
+//     const registerView = () =>
+//       fetch(`/api/views/${slug}`, {
+//         method: 'POST',
+//       })
+
+//     if (blogPage) {
+//       registerView()
+//     }
+//   }, [slug])
+
+//   return `${views > 0 ? views.toLocaleString() : '–––'} views`
+// }
+
 export const NavBar = () => {
   const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);

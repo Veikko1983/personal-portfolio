69b3dbd5133e4f88d6f48c83a092108b87ee2bec
Veikko1983 <veikko1983@gmail.com>
2023-08-19 11:48:01 +0300

    skills.js useless f code clean & new wikipage open

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -52,79 +52,79 @@ export const Skills = () => {
     e.target.style.height = "120px";
   }
   // FUNCTIONS TO WIKIPAGES //
-  function cSharpWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location =
-      "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
-  }
-  function javascriptWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://fi.wikipedia.org/wiki/JavaScript";
-  }
-  function dartWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location =
-      "https://en.wikipedia.org/wiki/Dart_(programming_language)";
-  }
-  function reactWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/React_(software)";
-  }
-  function nextJsWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/Next.js";
-  }
-  function cssWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/CSS";
-  }
-  function htmlWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/HTML";
-  }
-  function muiWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://mui.com/";
-  }
-  function bootstrapWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location =
-      "https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)";
-  }
-  function azureDevOpsWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/Azure_DevOps_Server";
-  }
-  function sourceTreeWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://www.sourcetreeapp.com/";
-  }
-  function nodeJsWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/Node.js";
-  }
-  function gitHubWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/GitHub";
-  }
-  function unityWiki(e) {
-    e.preventDefault();
-    window.open("https://veikko1983.github.io/personal-portfolio/");
-    window.location = "https://en.wikipedia.org/wiki/Unity_(game_engine)";
-  }
+  // function cSharpWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location =
+  //     "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)";
+  // }
+  // function javascriptWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://fi.wikipedia.org/wiki/JavaScript";
+  // }
+  // function dartWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location =
+  //     "https://en.wikipedia.org/wiki/Dart_(programming_language)";
+  // }
+  // function reactWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/React_(software)";
+  // }
+  // function nextJsWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/Next.js";
+  // }
+  // function cssWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/CSS";
+  // }
+  // function htmlWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/HTML";
+  // }
+  // function muiWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://mui.com/";
+  // }
+  // function bootstrapWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location =
+  //     "https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)";
+  // }
+  // function azureDevOpsWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/Azure_DevOps_Server";
+  // }
+  // function sourceTreeWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://www.sourcetreeapp.com/";
+  // }
+  // function nodeJsWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/Node.js";
+  // }
+  // function gitHubWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/GitHub";
+  // }
+  // function unityWiki(e) {
+  //   e.preventDefault();
+  //   window.open("https://veikko1983.github.io/personal-portfolio/");
+  //   window.location = "https://en.wikipedia.org/wiki/Unity_(game_engine)";
+  // }
   return (
     <section className="skill" id="skills">
       <Container>
@@ -139,158 +139,225 @@ export const Skills = () => {
                 className="skill-slider"
               >
                 <div className="item">
-                  <img
-                    onClick={cSharpWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={csharp}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
+                  >
+                    <img
+                      //onClick={cSharpWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={csharp}
+                      alt="Image"
+                    />
+                  </a>
 
                   <h5>C#</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={javascriptWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={javascript}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://fi.wikipedia.org/wiki/JavaScript"
+                  >
+                    <img
+                      //onClick={javascriptWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={javascript}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>Javascript</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={dartWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={dart}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/Dart_(programming_language)"
+                  >
+                    <img
+                      //onClick={dartWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={dart}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>Dart</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={reactWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={react}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/React_(software)"
+                  >
+                    <img
+                      // onClick={reactWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={react}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>React</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={nextJsWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={nextjs}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/Next.js"
+                  >
+                    <img
+                      // onClick={nextJsWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={nextjs}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>Next.js</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={cssWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={css}
-                    alt="Image"
-                  />
+                  <a target="blank" href="https://en.wikipedia.org/wiki/CSS">
+                    <img
+                      //onClick={cssWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={css}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>Css</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={htmlWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={html5}
-                    alt="Image"
-                  />
+                  <a target="blank" href="https://en.wikipedia.org/wiki/HTML">
+                    <img
+                      //onClick={htmlWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={html5}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>HTML5</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={muiWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={mui}
-                    alt="Image"
-                  />
+                  <a target="blank" href="https://mui.com/">
+                    <img
+                      //onClick={muiWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={mui}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>MUI React Component Library</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={bootstrapWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={bootstrap}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/Bootstrap_(front-end_framework)"
+                  >
+                    <img
+                      //onClick={bootstrapWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={bootstrap}
+                      alt="Image"
+                    />
+                  </a>
                   <h5>Bootstrap</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={azureDevOpsWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={azuredevops}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/Azure_DevOps_Server"
+                  >
+                    <img
+                      //onClick={azureDevOpsWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={azuredevops}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>AzureDevOps</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={sourceTreeWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={sourcetree}
-                    alt="Image"
-                  />
+                  <a target="blank" href="https://www.sourcetreeapp.com/">
+                    <img
+                      // onClick={sourceTreeWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={sourcetree}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>SourceTree</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={nodeJsWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={nodejs}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/Node.js"
+                  >
+                    <img
+                      // onClick={nodeJsWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={nodejs}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>Nodejs</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={gitHubWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={github}
-                    alt="Image"
-                  />
+                  <a target="blank" href="https://en.wikipedia.org/wiki/GitHub">
+                    <img
+                      // onClick={gitHubWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={github}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>GitHub</h5>
                 </div>
                 <div className="item">
-                  <img
-                    onClick={unityWiki}
-                    onMouseOver={changeBackground}
-                    onMouseOut={changeBackgroundToNormal}
-                    style={imageSize}
-                    src={unity}
-                    alt="Image"
-                  />
+                  <a
+                    target="blank"
+                    href="https://en.wikipedia.org/wiki/Unity_(game_engine)"
+                  >
+                    <img
+                      // onClick={unityWiki}
+                      onMouseOver={changeBackground}
+                      onMouseOut={changeBackgroundToNormal}
+                      style={imageSize}
+                      src={unity}
+                      alt="Image"
+                    />
+                  </a>
+
                   <h5>UNITY</h5>
                 </div>
               </Carousel>
